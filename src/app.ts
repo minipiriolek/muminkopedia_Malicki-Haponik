db996960848bb96ec1e69ad5644651de1b0cf677
szymonh <szymon.haponik@cyfroweszkoly.pl>
2026-04-01 09:09:25 +0200

    [UPDATE] implement artifact router

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import path from "path";
 import dotenv from "dotenv";
-
+import artifactRoutes from "./routes/artifactRoutes";
 dotenv.config();
 
 const app = express();
@@ -11,6 +11,8 @@ app.use(cors());
 app.use(express.json()); // Parsowanie JSON w requestach
 
 app.use(express.static(path.join(__dirname, "../public")));
+
+app.use('/artifact', artifactRoutes)
 
 app.get("/", (req, res) => {
     res.json({ message: "API Express + TypeScript działa!" });
